c293fe779148c03a37fdd1cb5699513e80547c38
Alimen <alimen@gmail.com>
2013-01-05 19:46:39 +0800

    Setup basic states & Add background

--- Title/Title.js
+++ Title/Title.js
@@ -13,6 +13,10 @@ function canvasApp() {
 	} else {
 		var theCanvas = document.getElementById("canvas");
 		var context = theCanvas.getContext("2d");
+		var backCanvas  = document.createElement("canvas");
+		backCanvas.width = theCanvas.width;
+		backCanvas.height = theCanvas.height;
+		var backContext = backCanvas.getContext("2d");		
 	}
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -28,8 +32,17 @@ function canvasApp() {
 	// Game states
 	const stateInitial = 0;
 	const stateLoading = 1;
-	const stateTitle = 2;
+	const stateReset = 2;
+	const stateTitle = 3;
 	var state = stateInitial;
+
+	// Loader variables
+	var itemsToLoad = 2;
+	var loadCount = 0;
+
+	// Image resources
+	var imgBackground = new Image();
+	var imgTiles = new Image();
 
 	// General variables
 	var mouseX = 0;
@@ -40,6 +53,13 @@ function canvasApp() {
 // Event functions
 //
 ///////////////////////////////////////////////////////////////////////////////
+
+	function eventItemLoaded(e) {
+		loadCount = loadCount + 1;
+		if(loadCount == itemsToLoad) {
+			state = stateReset;
+		}
+	}
 
 	function eventMouseMove(e) {
 		if(e.offsetX || e.offsetX == 0) {
@@ -65,7 +85,9 @@ function canvasApp() {
 			break;
 		case stateLoading:
 			drawLoading();
-			state = stateTitle;
+			break;
+		case stateReset:
+			reset();
 			break;
 		case stateTitle:
 			drawTitle();
@@ -75,15 +97,48 @@ function canvasApp() {
 
 	// Initializations
 	function init() {
-		state = stateLoading;
+		imgBackground.src = "WhiteRoom.jpg";
+		imgBackground.onload = eventItemLoaded;
+
+		imgTiles.src = "Tile.png";
+		imgTiles.onload = eventItemLoaded;
+
+		if(itemsToLoad != 0) {
+			state = stateLoading;
+		} else {
+			state = stateReset;
+		}
 	}
 
 	// Loading screen
 	function drawLoading() {
+		var percentage = Math.round(loadCount / itemsToLoad * 100);
+
+		// Clear Background
+		context.fillStyle = "#FFFFFF";
+		context.fillRect(0, 0, screenWidth, screenHeight);
+
+		// Print percentage
+		context.textBaseline = "bottom";	
+		context.fillStyle = "#000000";
+		context.font = "14px monospace";
+		context.textAlign = "center";
+		context.fillText(percentage + "%", screenWidth / 2, screenHeight / 2);
+	}
+
+	function reset() {
+		state = stateTitle;
 	}
 
 	// Title screen
 	function drawTitle() {
+		// Clear background
+		backContext.drawImage(imgBackground, 0, 0);
+
+		// Flip
+		context.drawImage(backCanvas, 0, 0);
+
+		// Debug message
 		context.textBaseline = "top";	
 		context.fillStyle = "#000000";
 		context.font = "14px monospace";
