0e7f63fd7af160cbdd71c33314ae49d14d0e4ca0
Alimen <alimen@gmail.com>
2013-01-20 21:32:21 +0800

    Add drawPanel()

--- Panels/Panels.js
+++ Panels/Panels.js
@@ -13,6 +13,10 @@ function canvasApp() {
 	} else {
 		var theCanvas = document.getElementById("canvas");
 		var context = theCanvas.getContext("2d");
+		var backCanvas  = document.createElement("canvas");
+		backCanvas.width = theCanvas.width;
+		backCanvas.height = theCanvas.height;
+		var backContext = backCanvas.getContext("2d");
 	}
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -33,11 +37,20 @@ function canvasApp() {
 	var state = stateInitial;
 
 	// Loader variables
-	var itemsToLoad = 1;
+	var itemsToLoad = 3;
 	var loadCount = 0;
 
 	// Image resources
-	var imgTile = new Image();
+	var imgBackground = new Image();
+	var imgPanel = new Image();
+	var imgBottons = new Image();
+
+	// Panel variables
+	var panelState;
+	var panelT;
+	var panelX, panelY;
+	var bottonShowed;
+	const bottonW = 80, bottonH = 20;
 
 	// General variables
 	var mouseX = 0;
@@ -67,6 +80,15 @@ function canvasApp() {
 	}
 	
 	function eventMouseClick(e) {
+		if(panelState == 0) {
+			panelX = mouseX;
+			panelY = mouseY - 80;
+			panelT = 0;
+			panelState = 1;
+		} else if(panelState == 2) {
+			panelT = 80;
+			panelState = 3;
+		}
 	}
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -100,8 +122,12 @@ function canvasApp() {
 		theCanvas.addEventListener("click", eventMouseClick, true);
 
 		// Setup image loader events
-		imgTile.src = "Tile.png";
-		imgTile.onload = eventItemLoaded;
+		imgBackground.src = "WhiteRoom.jpg";
+		imgBackground.onload = eventItemLoaded;
+		imgPanel.src = "Panel.png";
+		imgPanel.onload = eventItemLoaded;
+		imgBottons.src = "Bottons.png";
+		imgBottons.onload = eventItemLoaded;
 
 		// Switch to next state
 		if(itemsToLoad != 0) {
@@ -129,14 +155,21 @@ function canvasApp() {
 	}
 
 	function reset() {
+		panelState = 0;
+		bottonShowed = [0, 1, 2];
 		state = stateTitle;
 	}
 
 	// Title screen
 	function drawTitle() {
-		// Clear Background
-		context.fillStyle = "#FFFFFF";
-		context.fillRect(0, 0, screenWidth, screenHeight);
+		// Clear background
+		backContext.drawImage(imgBackground, 0, 0);
+
+		// Draw panel
+		drawPanel();
+
+		// Flip
+		context.drawImage(backCanvas, 0, 0);
 
 		// Debug message
 		context.textBaseline = "top";	
@@ -144,6 +177,25 @@ function canvasApp() {
 		context.font = "14px monospace";
 		context.textAlign = "right";
 		context.fillText("so far so good!", screenWidth, 0);
+		context.fillText("mouse = (" + mouseX + ", " + mouseY + ")", screenWidth , 15);
+		context.fillText("panelState = " + panelState, screenWidth, 30);
+	}
+
+	function drawPanel() {
+		if(panelState == 0) {
+			return;
+		} else if(panelState == 2) {
+			backContext.drawImage(imgPanel, panelX, panelY);
+			backContext.drawImage(imgBottons, 0, bottonH * bottonShowed[0], bottonW, bottonH, panelX+10, panelY+10, bottonW, bottonH);
+			backContext.drawImage(imgBottons, 0, bottonH * bottonShowed[1], bottonW, bottonH, panelX+10, panelY+35, bottonW, bottonH);
+			backContext.drawImage(imgBottons, 0, bottonH * bottonShowed[2], bottonW, bottonH, panelX+10, panelY+60, bottonW, bottonH);
+		} else if(panelState == 1 || panelState == 3) {
+			backContext.drawImage(imgPanel, 0, 0, 10, 10, panelX, panelY+70, 10, 10);
+			backContext.drawImage(imgPanel, 0, 80, 10, 10, panelX, panelY+80, 10, 10);
+			backContext.drawImage(imgPanel, 90, 0, 10, 10, panelX+10, panelY+70, 10, 10);
+			backContext.drawImage(imgPanel, 90, 80, 10, 10, panelX+10, panelY+80, 10, 10);
+			
+		}
 	}
 
 	const FPS = 30;
